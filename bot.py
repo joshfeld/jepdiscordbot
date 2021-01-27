28604c468b565f0afa49d381b3be5ec85b5f8835
kemianbd <feldmanj@gmail.com>
2021-01-27 12:49:34 -0500

    transition to database, update transaction and show tables and amounts, spoiler questions and answers

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 import discord
 import asyncio
-import pandas as pd
 from dotenv import load_dotenv
 
 from user import User
@@ -13,8 +12,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.clues = None
-        self.userdb = None
         self.u = None
 
     async def on_ready(self):
@@ -28,25 +25,28 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith('$load') and not message.guild:
+        if message.content.startswith('$load'):
             try:
                 await message.channel.send('Loading new questions')
-                self.clues = pd.read_csv('clues.csv')
                 self.u = User(message.author.id)
                 self.u.get_record()
             except:
                 await message.channel.send('Error loading clue data')
 
-        if message.content.startswith('$ask') and not message.guild:
-            if self.clues is not None and self.u is not None:
-                await message.channel.send(f'Category: {self.clues.iloc[self.u.day, 1]}')
-                await message.channel.send(self.clues.iloc[self.u.day, 3])
-                answer = self.clues.iloc[self.u.day, 4]
+        if message.content.startswith('$ask'):
+            if self.u is not None:
+                q = self.u.get_question
+                await message.channel.send(f'Category: ||{q[4]}||')
+                await message.channel.send(f'Dollar amount: {q[5]}')
+                await message.channel.send(f'||{q[6]}||')
+                answer = q[7]
                 try:
                     guess = await self.wait_for('message', timeout=120.0)
+                    await asyncio.sleep(0.5)
+                    await message.channel.purge(limit=1)
                 except asyncio.TimeoutError:
-                    self.u.update_record(cash=-int(self.clues.iloc[self.u.day, 2]))
-                    return await message.channel.send(f"Time's up, the correct answer is {answer}")
+                    self.u.update_record(clue_id=q[0], show_id=q[1], jep_round=q[3], cash=-int(q[5].replace('$', '')))
+                    return await message.channel.send(f"Time's up, the correct answer is ||{answer}||")
 
                 def check_answer(g, a):
                     prefix = ['what is ', 'who is ', 'where is ']
@@ -59,10 +59,13 @@ class MyClient(discord.Client):
 
                 if result:
                     await message.channel.send('Correct!')
-                    self.u.update_record(cash=int(self.clues.iloc[self.u.day, 2]))
+                    self.u.update_record(clue_id=q[0], show_id=q[1], jep_round=q[3], cash=int(q[5].replace('$', '')))
                 else:
-                    await message.channel.send(f'Wrong, the correct answer is {answer}')
-                    self.u.update_record(cash=-int(self.clues.iloc[self.u.day, 2]))
+                    await message.channel.send(f'Wrong, the correct answer is ||{answer}||')
+                    self.u.update_record(clue_id=q[0], show_id=q[1], jep_round=q[3], cash=-int(q[5].replace('$', '')))
+
+            else:
+                await message.channel.send('Please use $load to load your user data first')
 
 
 intents = discord.Intents.default()
--- user.py
+++ user.py
@@ -1,36 +1,93 @@
-import pandas as pd
+import sqlite3
 
 
 class User:
     def __init__(self, ident):
-        self.id = f"'{str(ident)}'"
+        self.id = ident
         self.userdb = None
         self.day = None
         self.winnings = None
+        self.show_winnings = None
 
     def get_record(self):
-        self.userdb = pd.read_csv('users.csv')
-        if self.id not in self.userdb['id'].values.tolist():
-            self.day = 0
-            self.winnings = 0
-            self.save(new=True)
-        else:
-            record = list(self.userdb[self.userdb['id'] == self.id].values)[0]
-            self.day = record[1]
-            self.winnings = record[2]
+        with sqlite3.connect('jepbot.db') as conn:
+            cur = conn.cursor()
+            cur.row_factory = lambda cursor, row: row[0]
+            if self.id not in cur.execute("SELECT discord_id FROM user").fetchall():
+                self.day = 1
+                self.winnings = 0
+                self.show_winnings = 0
+                self.new_user()
+            else:
+                self.day = cur.execute(f"SELECT questions_answered FROM user WHERE discord_id = {self.id}").fetchone()
+                self.winnings = cur.execute(f"SELECT lifetime_winnings FROM user WHERE discord_id = {self.id}").fetchone()
+                self.show_winnings = cur.execute(f"SELECT show_winnings FROM user WHERE discord_id = {self.id}").fetchone()
 
-    def update_record(self, cash=0):
+    def new_user(self):
+        with sqlite3.connect('jepbot.db') as conn:
+            nu = (self.id, 'TestName', 1, 0, 0)
+            conn.cursor().execute("""INSERT INTO user(discord_id, name, questions_answered, lifetime_winnings, show_winnings)
+                VALUES(?, ?, ?, ?, ?);""", nu)
+
+    def update_record(self, clue_id=None, show_id=None, jep_round=None, cash=0):
         self.day = self.day + 1
         self.winnings = self.winnings + cash
-        self.save()
+        self.show_winnings = self.show_winnings + cash
+        correct = True if cash > 0 else False
+        with sqlite3.connect('jepbot.db') as conn:
+            cur = conn.cursor()
 
-    def save(self, new=False):
-        if new:
-            self.userdb.loc[len(self.userdb)] = [self.id, 0, 0]
-        else:
-            self.userdb[self.userdb['id'] == self.id] = [[self.id, self.day, self.winnings]]
-        self.userdb.to_csv('users.csv', index=False)
+            # Update user table with result of question
+            cur.execute(f"UPDATE user SET questions_answered = {self.day} WHERE discord_id = {self.id}")
+            cur.execute(f"UPDATE user SET lifetime_winnings = {self.winnings} WHERE discord_id = {self.id}")
+            cur.execute(f"UPDATE user SET show_winnings = {self.show_winnings} WHERE discord_id = {self.id}")
+
+            # Add entry to transactions table from question asked
+            if clue_id is not None and show_id is not None:
+                transact = (self.get_user_id, clue_id, show_id, correct, cash)
+                cur.execute("""INSERT INTO transactions(user_id, clue_id, show_id, answered_correctly, winnings_change)
+                    VALUES(?, ?, ?, ?, ?);""", transact)
+
+            # If it was a final jeopardy clue, update the show table and reset show_winnings
+            if jep_round == 'Final Jeopardy!':
+                show_add = (show_id, self.get_user_id, self.show_winnings)
+                cur.execute("""INSERT INTO show(show_id, user_id, amount_won)
+                    VALUES(?, ?, ?);""", show_add)
+                self.show_winnings = 0
+                cur.execute(f"UPDATE user SET show_winnings = {self.show_winnings} WHERE discord_id = {self.id}")
 
     @property
     def print_record(self):
         return [self.id, self.day, self.winnings]
+
+    @property
+    def get_user_id(self):
+        with sqlite3.connect('jepbot.db') as conn:
+            user_id = conn.cursor().execute(f"SELECT user_id FROM user WHERE discord_id = {self.id}").fetchone()[0]
+        return user_id
+
+    @property
+    def get_question(self):
+        with sqlite3.connect('jepbot.db') as conn:
+            q = list(conn.cursor().execute(f"SELECT * FROM clues WHERE clue_id = {self.day}").fetchall()[0])
+        return q
+
+    @property
+    def best_show(self):
+        with sqlite3.connect('jepbot.db') as conn:
+            winnings = list(conn.cursor().execute(f"SELECT * FROM show WHERE user_id = {self.get_user_id}").fetchall()[0])
+            if winnings:
+                best = max(winnings)
+            else:
+                best = "No shows completed"
+        return best
+
+    @property
+    def worst_show(self):
+        with sqlite3.connect('jepbot.db') as conn:
+            winnings = list(conn.cursor().execute(f"SELECT * FROM show WHERE user_id = {self.get_user_id}").fetchall()[0])
+            if winnings:
+                worst = min(winnings)
+            else:
+                worst = "No shows completed"
+        return worst
