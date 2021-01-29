a374acd0f2e9a61ef23e9c9d8de54c7b76b04360
kemianbd <feldmanj@gmail.com>
2021-01-28 19:15:19 -0500

    expand transactions, add dispute command, ability to check winnings

--- bot.py
+++ bot.py
@@ -25,16 +25,19 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
+        # Loads the user's record from the user table
         if message.content.startswith('$load'):
             try:
                 await message.channel.send('Loading new questions')
-                self.u = User(message.author.id)
+                self.u = User(message.author.id, message.author.name)
                 self.u.get_record()
             except:
                 await message.channel.send('Error loading clue data')
 
+        # Asks the next question for the user, gathers the answer, then updates all info
         if message.content.startswith('$ask'):
             if self.u is not None:
+                guess = None
                 q = self.u.get_question
                 await message.channel.send(f'Category: ||{q[4]}||')
                 await message.channel.send(f'Dollar amount: {q[5]}')
@@ -45,7 +48,8 @@ class MyClient(discord.Client):
                     await asyncio.sleep(0.5)
                     await message.channel.purge(limit=1)
                 except asyncio.TimeoutError:
-                    self.u.update_record(clue_id=q[0], show_id=q[1], jep_round=q[3], cash=-int(q[5].replace('$', '')))
+                    self.u.update_record(question=q[6], answer=answer, guess=guess, clue_id=q[0], show_id=q[1],
+                                         jep_round=q[3], cash=-int(q[5].replace('$', '')))
                     return await message.channel.send(f"Time's up, the correct answer is ||{answer}||")
 
                 def check_answer(g, a):
@@ -59,13 +63,40 @@ class MyClient(discord.Client):
 
                 if result:
                     await message.channel.send('Correct!')
-                    self.u.update_record(clue_id=q[0], show_id=q[1], jep_round=q[3], cash=int(q[5].replace('$', '')))
+                    self.u.update_record(question=q[6], answer=answer, guess=guess.content, clue_id=q[0], show_id=q[1],
+                                         jep_round=q[3], cash=int(q[5].replace('$', '')))
                 else:
                     await message.channel.send(f'Wrong, the correct answer is ||{answer}||')
-                    self.u.update_record(clue_id=q[0], show_id=q[1], jep_round=q[3], cash=-int(q[5].replace('$', '')))
+                    self.u.update_record(question=q[6], answer=answer, guess=guess.content, clue_id=q[0], show_id=q[1],
+                                         jep_round=q[3], cash=-int(q[5].replace('$', '')))
 
             else:
                 await message.channel.send('Please use $load to load your user data first')
+
+        # Allows the user to check their winnings
+        if message.content.startswith('$winnings'):
+            if self.u is not None:
+                await message.channel.send(f'Total lifetime winnings for {self.u.discord_name}: {self.u.winnings}')
+                await message.channel.send(f'Current show winnings for {self.u.discord_name}: {self.u.show_winnings}')
+            else:
+                await message.channel.send('Please use $load to load your user data first')
+
+        # Sends me a DM if a user disputes their answer
+        if message.content.startswith('$dispute'):
+            if self.u is not None:
+                try:
+                    await message.channel.send('Your last response will be sent for manual review')
+                    await client.get_user(261512986949058560).send(f'Clue disputed by {message.author.name} - '
+                                                                   f'clue id {self.u.day - 1}')
+                except:
+                    await message.channel.send("Looks like you've got nothing to dispute")
+            else:
+                await message.channel.send('Please use $load to load your user data first')
+
+        # Debug
+        if message.content.startswith('$test'):
+            await message.channel.send(f'Name: {message.author.name}, ID: {message.author.id}')
+            await client.get_user(message.author.id).send("Test DM")
 
 
 intents = discord.Intents.default()
--- user.py
+++ user.py
@@ -2,12 +2,13 @@ import sqlite3
 
 
 class User:
-    def __init__(self, ident):
+    def __init__(self, ident, discord_name):
         self.id = ident
         self.userdb = None
         self.day = None
         self.winnings = None
         self.show_winnings = None
+        self.discord_name = discord_name
 
     def get_record(self):
         with sqlite3.connect('jepbot.db') as conn:
@@ -25,11 +26,11 @@ class User:
 
     def new_user(self):
         with sqlite3.connect('jepbot.db') as conn:
-            nu = (self.id, 'TestName', 1, 0, 0)
+            nu = (self.id, self.discord_name, 1, 0, 0)
             conn.cursor().execute("""INSERT INTO user(discord_id, name, questions_answered, lifetime_winnings, show_winnings)
                 VALUES(?, ?, ?, ?, ?);""", nu)
 
-    def update_record(self, clue_id=None, show_id=None, jep_round=None, cash=0):
+    def update_record(self, question=None, answer=None, guess=None, clue_id=None, show_id=None, jep_round=None, cash=0):
         self.day = self.day + 1
         self.winnings = self.winnings + cash
         self.show_winnings = self.show_winnings + cash
@@ -44,9 +45,9 @@ class User:
 
             # Add entry to transactions table from question asked
             if clue_id is not None and show_id is not None:
-                transact = (self.get_user_id, clue_id, show_id, correct, cash)
-                cur.execute("""INSERT INTO transactions(user_id, clue_id, show_id, answered_correctly, winnings_change)
-                    VALUES(?, ?, ?, ?, ?);""", transact)
+                transact = (self.get_user_id, clue_id, show_id, correct, cash, question, answer, guess)
+                cur.execute("""INSERT INTO transactions(user_id, clue_id, show_id, answered_correctly, winnings_change, question, answer, guess)
+                    VALUES(?, ?, ?, ?, ?, ?, ?, ?);""", transact)
 
             # If it was a final jeopardy clue, update the show table and reset show_winnings
             if jep_round == 'Final Jeopardy!':
